63c8616904a4e7c782edd5ecaaa79358ad59e345
Enno Siemsen <siemsene@gmail.com>
2026-03-05 15:08:20 +0000

    Improved admin functions

--- functions/lib/profit.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.profitForDay = profitForDay;
-function profitForDay(D, Q, price, cost, salvage) {
-    const sold = Math.min(Q, D);
-    const leftover = Math.max(0, Q - sold);
-    return price * sold + salvage * leftover - cost * Q;
-}
